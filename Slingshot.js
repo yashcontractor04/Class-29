a267ee7bc1a295fc7ae6421807904b90dff4bfba
yashcontractor04 <yashcontractor04@gmail.com>
2020-07-27 17:49:02 +0530

    Class29

--- Slingshot.js
+++ Slingshot.js
@@ -8,6 +8,9 @@ class SlingShot{
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
+        this.sling1=loadImage("sprites/sling1.png");
+        this.sling2=loadImage("sprites/sling2.png");
+        this.sling3=loadImage("sprites/sling3.png");
         World.add(world, this.sling);
     }
 
@@ -19,9 +22,23 @@ class SlingShot{
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
+            stroke(85,42,19);    
             strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            if(pointA.x<220){
+            line(pointA.x-20, pointA.y, pointB.x, pointB.y);
+            line(pointA.x, pointA.y, pointB.x+40, pointB.y-5);
+            image(this.sling3,pointA.x-28,pointA.y-10,15,20);
+            }
+            else{
+            line(pointA.x+20, pointA.y, pointB.x, pointB.y);
+            line(pointA.x+20, pointA.y, pointB.x+40, pointB.y-5);
+            image(this.sling3,pointA.x+20,pointA.y+5,15,20);
+            }
         }
+        image(this.sling1,220,70,40,150);
+        image(this.sling2,195,65,40,100);
+        
+
     }
     
 }
